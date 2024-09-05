27cb74dccb80b2f83afbd1389ad52485464ae786
bandido157ladrao <143723670+bandido157ladrao@users.noreply.github.com>
2024-09-05 12:11:00 -0300

    Add files via upload

---
+++ modelos/alunos.py
@@ -0,0 +1,12 @@
+class Aluno:
+    nome = ''
+    idade = ''
+    turno = ''
+    matricula = False
+    
+aluno_Reis = Aluno()
+aluno_Bia  = Aluno()
+
+alunos = (aluno_Reis, aluno_Bia)
+
+print(alunos)       
