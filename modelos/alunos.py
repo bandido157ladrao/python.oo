aa158ad7f701a2f9a8d02018b2fd938227379c16
bandido157ladrao <143723670+bandido157ladrao@users.noreply.github.com>
2024-09-13 10:39:47 -0300

    Add files via upload

--- modelos/alunos.py
+++ modelos/alunos.py
@@ -1,10 +1,20 @@
 class Aluno:
+    alunos=[]
+
     def __init__(self, nome, idade, turno):
         self.nome = nome
         self.idade = idade
         self.turno = turno
         self.matricula = False
+        Aluno.alunos.append(self)
 
+    def __str__(self):
+        return {f'{self.nome} | {self.idade} | {self.turno}'}
+                                               
+    def listar_alunos():                                       
+        for alunos in Aluno.alunos:                                       
+        print(f'{cliente.nome} | {cliente.idade} | {cliente.turno}')                                      
+                                              
 aluno_Reis = Aluno('Eduardo', '17', 'Manha')
 aluno_Bia = Aluno('Bia', '17', 'Tarde')
 
