b4c6a8f0968a8c065615c87ca841195d1146a501
bandido157ladrao <143723670+bandido157ladrao@users.noreply.github.com>
2024-09-18 09:16:40 -0300

    Update alunos.py

--- modelos/alunos.py
+++ modelos/alunos.py
@@ -1,25 +1,37 @@
-class Aluno:
-    alunos=[]
+class Alunos:
+    alunos = []
 
-    def __init__(self, nome, idade, turno):
+    def __init__(self, nome, idade, objetivo, local):
         self.nome = nome
         self.idade = idade
-        self.turno = turno
-        self.matricula = False
-        Aluno.alunos.append(self)
+        self.objetivo = objetivo
+        self.local = local
+        self._matriculado = True
+        Alunos.alunos.append(self)
 
     def __str__(self):
-        return {f'{self.nome} | {self.idade} | {self.turno}'}
-                                               
-    def listar_alunos():                                       
-        for alunos in Aluno.alunos:                                       
-        print(f'{cliente.nome} | {cliente.idade} | {cliente.turno}')                                      
-                                              
-aluno_Reis = Aluno('Eduardo', '17', 'Manha')
-aluno_Bia = Aluno('Bia', '17', 'Tarde')
+        return f'{self.nome} | {self.idade} | {self.objetivo} | {self.local} | {self.matriculado}'
 
-aluno_Reis.nome = 'Reis'
-aluno_Bia.nome = 'Bia'
+    @staticmethod
+    def listar_alunos():
+        print('')        
+        print('''
+                             ▄▀█ █░░ █░█ █▄░█ █▀█ █▀   █▀▄ ▄▀█   █▀█ █▀▀ █ █▀   █▀▀ █▄█ █▀▄▀█
+                             █▀█ █▄▄ █▄█ █░▀█ █▄█ ▄█   █▄▀ █▀█   █▀▄ ██▄ █ ▄█   █▄█ ░█░ █░▀░█''')
+        
+        print(f'{"Nome do Aluno".ljust(20)} | {"Idade".ljust(6)} | {"Objetivo".ljust(20)} | {"Local".ljust(20)} | {"Matrícula"}')
+        print('------------------------------------------------------------------------------------------------------------------------------')
+        for aluno in Alunos.alunos:
+            print(f'{aluno.nome.ljust(20)} | {str(aluno.idade).ljust(6)} | {aluno.objetivo.ljust(20)} | {aluno.local.ljust(20)} | {aluno.matriculado}')
+            print('-------------------------------------------------------------------------------------------------------------------------')
 
-alunos = [aluno_Reis, aluno_Bia]
+    @property
+    def matriculado(self):
+        return '✅ Matriculado' if self._matriculado else '❌ Não Matriculado'        
+
+aluno_joao = Alunos('Reis', 25, 'Hipertrofia', 'Academia A')
+aluno_maria = Alunos('Vaida', 50, 'Emagrecimento', 'Academia B')
+aluno_pedro = Alunos('Bia', 22, 'Resistência', 'Academia A')
+
+Alunos.listar_alunos()
 
